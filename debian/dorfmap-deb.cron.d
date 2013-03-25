033bdb4ee23d95a80e734e238d07df80ba85866f
Daniel Friesel <derf@finalrewind.org>
2013-03-25 13:17:49 +0100

    initial commit

---
+++ debian/dorfmap-deb.cron.d
@@ -0,0 +1,8 @@
+# /etc/cron.d/dorfmap-helpers
+
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+MAILTO=root
+
+# m    h   dom mon dow user      command
+  */6  *   *   *   *   root      automatic_light_control
+  *    *   *   *   *   www-data  update_www_status
