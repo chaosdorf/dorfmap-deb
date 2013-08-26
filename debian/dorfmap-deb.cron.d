334286a7f6c32dce5ddafb085439b768afe8a00b
Daniel Friesel <derf@finalrewind.org>
2013-08-26 20:43:03 +0200

    add clocks and feedback3

--- debian/dorfmap-deb.cron.d
+++ debian/dorfmap-deb.cron.d
@@ -6,3 +6,4 @@ MAILTO=root
 # m    h   dom mon dow user      command
   */6  *   *   *   *   root      automatic_light_control
   *    *   *   *   *   www-data  update_www_status
+  *    *   *   *   *   www-data  update_clock_cron
