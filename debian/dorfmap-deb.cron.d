1f920994e163eaf895471175aa359b3592fc5b26
Daniel Friesel <derf@finalrewind.org>
2013-08-27 15:49:30 +0200

    separate clock update script

--- debian/dorfmap-deb.cron.d
+++ debian/dorfmap-deb.cron.d
@@ -6,4 +6,4 @@ MAILTO=root
 # m    h   dom mon dow user      command
   */6  *   *   *   *   root      automatic_light_control
   *    *   *   *   *   www-data  update_www_status
-  *    *   *   *   *   www-data  update_clock_cron
+  *    *   *   *   *   www-data  update_clocks
