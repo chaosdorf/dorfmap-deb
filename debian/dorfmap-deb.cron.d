2ccefeec5afbc74d956166fcd91672622d68d910
Daniel Friesel <derf@finalrewind.org>
2015-01-17 17:02:48 +0100

    raumstatus_update: push, don't be polled

--- debian/dorfmap-deb.cron.d
+++ debian/dorfmap-deb.cron.d
@@ -5,6 +5,7 @@ MAILTO=root
 
 # m    h   dom mon dow user      command
   */6  *   *   *   *   root      nice automatic_light_control
+  */6  *   *   *   *   root      nice raumstatus_update
   */4  *   *   *   *   www-data  nice gather_bgdata
   *    *   *   *   *   www-data  update_www_status
   *    *   *   *   *   www-data  update_clocks
